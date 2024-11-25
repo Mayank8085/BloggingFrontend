7a2a3f0ce10353449ed234d3dfff90eddae8d821
Mayank8085 <mayanksahu1806@gmail.com>
2024-11-25 23:21:55 +0530

    added loader

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,6 +5,9 @@ import Header from './Header'
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import { backend } from '../backend';
+import {
+  ColorRing
+} from "react-loader-spinner";
 const Home = () => {
   const [posts,setposts] = useState([]);
   const {search} = useLocation();
@@ -20,7 +23,12 @@ const Home = () => {
     <div>
       <Header/>
       <div style={{display:"flex"}}>
-        {posts.length ===0 ?<h4>Sorry!! No posts found </h4>: <Post posts={posts}/> }
+        {posts.length ===0 ? (<div>
+          <div className="loader ">
+            <ColorRing color="#00BFFF" height={100} width={100} />
+          </div>
+          <h1 style={{textAlign:"center"}}>Loading...</h1>
+        </div>): <Post posts={posts}/> }
         <Sidebar />
       </div>
     </div>
